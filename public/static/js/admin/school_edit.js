f6483a59c556d056317ae8ba5742686143640932
冰华 <3218366812@qq.com>
2018-01-30 22:02:12 +0800

    前台页面修改

---
+++ public/static/js/admin/school_edit.js
@@ -0,0 +1,83 @@
+/**
+ * 后台学校修改页面js管理
+ * @project 2018考研初试排名查询系统
+ * @author 冰华
+ * @since 2018-1-29 23:14:48
+ */
+var m = angular.module("ranking_admin_school_edit_application", []);
+m.controller(
+    "ranking_admin_school_edit_controller",
+    [
+        "$scope",
+        "$http",
+        function ($scope, $http){
+            $scope.page = 1;
+            $scope.spiner_example = true;
+            $scope.schoolArray = new Array();
+            /**
+             * 获取学校数据方法
+             * @method 调用
+             */
+            $http.get(
+                "/admin/school/school_list_data.html?page="
+            ).then(
+                function (result){
+                    $scope.data = result;
+                },
+                function (){
+                    layer.msg(
+                        "网络错误,请稍后重试",
+                        {
+                            anim : 2,
+                            time : 900
+                        },
+                        function (){
+                            window.location.reload(true);
+                        }
+                    );
+                }
+            );
+
+            /**
+             * 添加学校
+             * @method 调用
+             */
+            $scope.submitCollegesInfo = function (){
+                $http.post(
+                    "/admin/school/add_school_info",
+                    $scope.colleges
+                ).then(
+                    function (result){
+                        var respone_obj = result.data;
+                        layer.msg(
+                            respone_obj.msg,
+                            {
+                                anim : 2,
+                                time : 900
+                            },
+                            function () {
+                                if (respone_obj.code==1){
+                                    window.location.reload(true);
+                                    return false;
+                                }
+                            }
+                        );
+                    },
+                    function (){
+                        layer.msg(
+                            "网络错误,请稍后重试",
+                            {
+                                anim : 2,
+                                time : 900
+                            },
+                            function (){
+                                window.location.reload(true);
+                            }
+                        );
+                    }
+                );
+            };
+
+        }
+    ]
+);
--- runtime/temp/e00839faa94298f48532db17247cfe33.php
+++ runtime/temp/e00839faa94298f48532db17247cfe33.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:96:"D:\workspace\company-www\ranking-lunhui\public/../application/admin\view\school\school_edit.html";i:1517304313;s:81:"D:\workspace\company-www\ranking-lunhui\application\admin\view\public\header.html";i:1516956621;s:81:"D:\workspace\company-www\ranking-lunhui\application\admin\view\public\footer.html";i:1516956154;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:96:"D:\workspace\company-www\ranking-lunhui\public/../application/admin\view\school\school_edit.html";i:1517317734;s:81:"D:\workspace\company-www\ranking-lunhui\application\admin\view\public\header.html";i:1516956621;s:81:"D:\workspace\company-www\ranking-lunhui\application\admin\view\public\footer.html";i:1516956154;}*/ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,18 +25,15 @@
 </head>
 <link rel="stylesheet" type="text/css" href="/static/admin/webupload/webuploader.css">
 <link rel="stylesheet" type="text/css" href="/static/admin/webupload/style.css">
-<style>
-    .file-item{float: left; position: relative; width: 110px;height: 110px; margin: 0 20px 20px 0; padding: 4px;}
-    .file-item .info{overflow: hidden;}
-    .uploader-list{width: 100%; overflow: hidden;}
-</style>
-<body class="gray-bg">
+<script type="text/javascript" src="/static/admin/webupload/webuploader.min.js"></script>
+<script type="text/javascript" src="/static/js/admin/school_edit.js"></script>
+<body class="gray-bg" ng-app="ranking_admin_school_edit_application" ng-controller="ranking_admin_school_edit_controller">
 <div class="wrapper wrapper-content animated fadeInRight">
     <div class="row">
         <div class="col-sm-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <h5>编辑管理员</h5>
+                    <h5>编辑学校</h5>
                     <div class="ibox-tools">
                         <a class="collapse-link">
                             <i class="fa fa-chevron-up"></i>
@@ -49,68 +46,28 @@
                         </a>
                     </div>
                 </div>
-                <div class="ibox-content">
-                    <form class="form-horizontal" name="userEdit" id="userEdit" method="post" action="<?php echo url('userEdit'); ?>">
-                        <input type="hidden" name="id" value="">
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">管理员名称：</label>
-                            <div class="input-group col-sm-4">
-                                <input id="username" type="text" class="form-control" name="username" required="" aria-required="true" value="<?php echo $user['username']; ?>">
-                            </div>
+                <div class="ibox-content form-horizontal">
+                    <input type="hidden" name="id" value="">
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">学校名称:</label>
+                        <div class="input-group col-sm-4">
+                            <input type="text" class="form-control" required="" aria-required="true" ng-model="data.colleges_name"/>
                         </div>
-                        <div class="hr-line-dashed"></div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">管理员角色：</label>
-                            <div class="input-group col-sm-4">
-                                <select class="form-control m-b chosen-select" name="groupid" id="groupid">
-                                    <option value="">==请选择角色==</option>
-
-                                </select>
-                            </div>
+                    </div>
+                    <div class="hr-line-dashed"></div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label">QQ群号:</label>
+                        <div class="input-group col-sm-4">
+                            <input type="text" class="form-control" required="" aria-required="true" ng-model="data.qq_group"/>
                         </div>
-                        <div class="hr-line-dashed"></div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">头像：</label>
-                            <div class="input-group col-sm-4">
-                                <input type="hidden" id="data_photo" name="portrait" value="" />
-                                <div id="fileList" class="uploader-list" style="float:right"></div>
-                                <div id="imgPicker" style="float:left">选择头像</div>
-                                <img id="img_data" class="img-circle" height="80px" width="80px" style="float:left;margin-left: 50px;margin-top: -10px;" src="/uploads/face/<?php echo $user['portrait']; ?>" onerror="this.src='/static/admin/images/head_default.gif'"/>
-                            </div>
+                    </div>
+                    <div class="hr-line-dashed"></div>
+                    <div class="form-group">
+                        <div class="col-sm-4 col-sm-offset-3">
+                            <button class="btn btn-primary" type="button"><i class="fa fa-save"></i> 保存</button>&nbsp;&nbsp;&nbsp;
+                            <a class="btn btn-danger" href="javascript:history.go(-1);"><i class="fa fa-close"></i> 返回</a>
                         </div>
-                        <div class="hr-line-dashed"></div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">登录密码：</label>
-                            <div class="input-group col-sm-4">
-                                <input id="password" type="text" class="form-control" name="password"  placeholder="再次输入修改密码">
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">真实姓名：</label>
-                            <div class="input-group col-sm-4">
-                                <input id="real_name" type="text" class="form-control" name="real_name" required="" aria-required="true" value="<?php echo $user['real_name']; ?>">
-
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">状&nbsp;态：</label>
-                            <div class="col-sm-6">
-                                <div class="radio i-checks">
-                                    <input type="radio" name='status' value="1" <?php if($user['status'] == 1): ?>checked<?php endif; ?>/>开启&nbsp;&nbsp;
-                                    <input type="radio" name='status' value="0" <?php if($user['status'] == 0): ?>checked<?php endif; ?>/>关闭
-                                </div>
-                            </div>
-                        </div>
-                        <div class="hr-line-dashed"></div>
-                        <div class="form-group">
-                            <div class="col-sm-4 col-sm-offset-3">
-                                <button class="btn btn-primary" type="submit"><i class="fa fa-save"></i> 保存</button>&nbsp;&nbsp;&nbsp;
-                                <a class="btn btn-danger" href="javascript:history.go(-1);"><i class="fa fa-close"></i> 返回</a>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
 
@@ -133,107 +90,6 @@
 <script src="/static/js/admin/student_list_data.js"></script>
 <script>
     $(document).ready(function(){$(".i-checks").iCheck({checkboxClass:"icheckbox_square-green",radioClass:"iradio_square-green",})});
-</script>
-<script type="text/javascript" src="/static/admin/webupload/webuploader.min.js"></script>
-<script type="text/javascript">
-    var $list = $('#fileList');
-    //上传图片,初始化WebUploader
-    var uploader = WebUploader.create({
-
-        auto: true,// 选完文件后，是否自动上传。
-        swf: '/static/admin/webupload/Uploader.swf',// swf文件路径
-        server: "<?php echo url('Upload/uploadface'); ?>",// 文件接收服务端。
-        duplicate :true,// 重复上传图片，true为可重复false为不可重复
-        pick: '#imgPicker',// 选择文件的按钮。可选。
-
-        accept: {
-            title: 'Images',
-            extensions: 'gif,jpg,jpeg,bmp,png',
-            mimeTypes: 'image/jpg,image/jpeg,image/png'
-        },
-
-        'onUploadSuccess': function(file, data, response) {
-            $("#data_photo").val(data._raw);
-            $("#img_data").attr('src', '/uploads/face/' + data._raw).show();
-        }
-    });
-
-    uploader.on( 'fileQueued', function( file ) {
-        $list.html( '<div id="' + file.id + '" class="item">' +
-            '<h4 class="info">' + file.name + '</h4>' +
-            '<p class="state">正在上传...</p>' +
-            '</div>' );
-    });
-
-    // 文件上传成功
-    uploader.on( 'uploadSuccess', function( file ) {
-        $( '#'+file.id ).find('p.state').text('上传成功！');
-    });
-
-    // 文件上传失败，显示上传出错。
-    uploader.on( 'uploadError', function( file ) {
-        $( '#'+file.id ).find('p.state').text('上传出错!');
-    });
-
-    //提交
-    $(function(){
-        $('#userEdit').ajaxForm({
-            beforeSubmit: checkForm,
-            success: complete,
-            dataType: 'json'
-        });
-
-        function checkForm(){
-            if( '' == $.trim($('#username').val())){
-                layer.msg('请输入用户名',{icon:2,time:1500,shade: 0.1}, function(index){
-                    layer.close(index);
-                });
-                return false;
-            }
-
-            if( '' == $.trim($('#groupid').val())){
-                layer.msg('请选择用户角色',{icon:2,time:1500,shade: 0.1}, function(index){
-                    layer.close(index);
-                });
-                return false;
-            }
-
-            if( '' == $.trim($('#real_name').val())){
-                layer.msg('请输入真实姓名',{icon:2,time:1500,shade: 0.1}, function(index){
-                    layer.close(index);
-                });
-                return false;
-            }
-        }
-
-
-        function complete(data){
-            if(data.code==1){
-                layer.msg(data.msg, {icon: 6,time:1500,shade: 0.1}, function(index){
-                    window.location.href="<?php echo url('user/index'); ?>";
-                });
-            }else{
-                layer.msg(data.msg, {icon: 5,time:1500,shade: 0.1});
-                return false;
-            }
-        }
-
-    });
-
-
-
-    //IOS开关样式配置
-    var elem = document.querySelector('.js-switch');
-    var switchery = new Switchery(elem, {
-        color: '#1AB394'
-    });
-    var config = {
-        '.chosen-select': {},
-    }
-    for (var selector in config) {
-        $(selector).chosen(config[selector]);
-    }
-
 </script>
 </body>
 </html>
